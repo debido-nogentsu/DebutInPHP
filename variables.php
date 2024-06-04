c14c048e4f70c665f162e615ce65956f3ce5bbab
debido-nogentsu <163670067+debido-nogentsu@users.noreply.github.com>
2024-06-04 08:42:33 +0200

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,7 @@
+<?php
+// défintion de mes variables
+$title = "DebutInPHP";
+$ligne1 = "Ceci est le contenu principale de ma page.";
+$ligne2 = "C'est une page HTML toute simple contenant du code PHP.";
+$ligne3 = "Mais, cette fois-ci, j'ai utilis&eacute; des variables pour afficher ce texte.";
+?>
