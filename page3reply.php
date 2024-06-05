9415f0d394720b215e3041189f385ca26a7ee721
DEBIDO NOGENTSU <163670067+debido-nogentsu@users.noreply.github.com>
2024-06-05 08:50:55 +0200

    Update page3reply.php

--- page3reply.php
+++ page3reply.php
@@ -12,7 +12,7 @@ include_once(__DIR__ . '/variables.php');
     <head>
     <meta charset="UTF-8">
     <title>DebutInPHP</title>
-    <<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="my_css.css" rel="stylesheet">
     </head>
 
