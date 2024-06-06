f46d4a4a3e22e927694965e71f2721562da14440
DEBIDO NOGENTSU <163670067+debido-nogentsu@users.noreply.github.com>
2024-06-06 10:48:12 +0200

    Update mon_compte.php

--- mon_compte.php
+++ mon_compte.php
@@ -40,7 +40,7 @@ include_once(__DIR__ . '/variables.php');
                         echo $nom;
                         echo "</h1>";
                         echo "<h2>Vous &ecirc;tes ici chez vous !</h2>";
-                        echo "<h3><a href='logout.php'>QUITTER</a><h3>";
+                        echo "<h3><a href='page4.php'>QUITTER</a><h3>";
                 }
                 else { // sinon afficher ce message d'erreur
                     echo "Les donn&eacute;es saisies ne correspondent pas aux donn&eacute;es pr&eacute;d&eacute;finies.";
